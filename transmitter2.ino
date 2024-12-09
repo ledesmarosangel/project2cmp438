473b43b10d7949b1ef1615f4275f1aae64a48050
ledesmarosangel <137957251+ledesmarosangel@users.noreply.github.com>
2024-12-09 14:43:50 -0500

    Update transmitter2.ino

--- transmitter2.ino
+++ transmitter2.ino
@@ -1,7 +1,6 @@
 
 //HC-05  
 #include<SoftwareSerial.h>
-
 SoftwareSerial BTSerial(10, 11);
 
 //MPU6050
@@ -14,14 +13,12 @@ void setup(){
   BTSerial.begin(38400);
   Serial.begin(38400);
 
-//we got this from a tutorial
   //MPU6050
   Wire.begin();                                      //begin the wire communication
   Wire.beginTransmission(MPU_addr1);                 //begin, send the slave address (in this case 68)
   Wire.write(0x6B);                                  //make the reset (place a 0 into the 6B register)
   Wire.write(0);
   Wire.endTransmission(true);                        //end the transmission
-  
 }
 
 
@@ -29,33 +26,52 @@ void setup(){
 void loop(){
 
   mpuRead();
-  printValues(); 
+  printValues(); //- uncomment to this to callibrate the necessary values
   
-  if(roll<-2 || pitch>6){
+  if( (roll >= -125 && roll <= -80) &&  (pitch>=-65 && pitch<=-15)){
     BTSerial.write('0');
-    Serial.println("backward");
+    Serial.println("Reverse");
     delay(100);
     }
 
-  else if(roll>5 || pitch <-2){
+  else if((roll >= 60 && roll <= 140) &&  (pitch>=-80 && pitch<=-25)){
       BTSerial.write('1');
-      Serial.println("forward");
-      delay(100)0;
+      Serial.println("Forward");
+      delay(100);
+    }
+  else if ((roll >= -180 && roll <= -130) &&  (pitch>=-85 && pitch<=-68)){
+      BTSerial.write('2');
+      Serial.println("Stop");
+      delay(100);
     }
 
-else {
-      BTSerial.write('2');
-      Serial.println("stop");
-      delay(1000);
+  else if ((roll >= -100 && roll <= 25) &&  (pitch>=-100 && pitch<=-17)){
+      BTSerial.write('3');
+      Serial.println("Left");
+      delay(100);
     }
+
+  else if ((roll >= -190 && roll <= -135) &&  (pitch>=-35 && pitch<=18)){
+      BTSerial.write('4');
+      Serial.println("Right");
+      delay(100);
+  }
+
+
+  else{
+    BTSerial.write('2');
+      Serial.println("Stop");
+      delay(100);
+  }
+
+    
+
+
 
 delay(500);
 }
   
 
-//we got the following from a tutorial -rosangel & hamza
-
-//
 void mpuRead(){
   Wire.beginTransmission(MPU_addr1);
   Wire.write(0x3B);  //send starting register address, accelerometer high byte
@@ -72,7 +88,6 @@ void mpuRead(){
   pitch = atan2(-xa , sqrt(ya * ya + za * za)) * 180.0 / PI; //account for roll already applied
 }
 
-
 void printValues(){
   Serial.print("roll = ");
   Serial.print(roll,2);
@@ -83,5 +98,4 @@ void printValues(){
   Serial.print(", ya = ");
   Serial.println(ya, 2);
   delay(500);
-  //end of tutorial code
 }
